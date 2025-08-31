5396557428529a3a7df3494010c1e68e1ae887b7
adithya <adishetty1001@gmail.com>
2025-09-01 04:47:15 +0530

    modify file1 and file2

--- file1.py
+++ file1.py
@@ -1,1 +1,3 @@
 print("this is a file 1")
+
+print("lets modify the the file1 as well")
--- file2.py
+++ file2.py
@@ -1,1 +1,4 @@
 print("this is file 2")
+
+
+print("lets make a change to the file2 and push it to the remote")
